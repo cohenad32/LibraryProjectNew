0517e129cb9e550b5b2275ae1a29c57ac3f6eb70
Adina Cohen <adina.cohen2@mail.yu.edu>
2018-05-16 10:18:18 -0400

    edited main

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,10 +10,12 @@ import java.sql.*;
 
 public class Main extends Application {
 
-
+// uncomment out each line to test out each form
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("updateCustomer.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("createCustomer.fxml"));
+        // Parent root = FXMLLoader.load(getClass().getResource("updateCustomer.fxml"));
+        // Parent root = FXMLLoader.load(getClass().getResource("checkOutMaterial.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
